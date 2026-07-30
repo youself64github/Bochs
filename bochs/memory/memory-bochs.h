ab42492c2c5cb0dcbe04846941e2e4a8a6ea3609
youself64 <121099244+youself64github@users.noreply.github.com>
2026-07-30 22:40:53 +0300

    Remove init_memory overload without ips parameter
    Removed overloaded init_memory function without ips parameter from BX_MEMORY_STUB_C and BX_MEM_C classes.

--- bochs/memory/memory-bochs.h
+++ bochs/memory/memory-bochs.h
@@ -91,7 +91,6 @@ public:
   virtual ~BX_MEMORY_STUB_C();
 
   BX_MEM_SMF void    init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz, Bit64u ips);
-  BX_MEM_SMF void    init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz);
   BX_MEM_SMF void    cleanup_memory(void);
   BX_MEM_SMF Bit8u*  get_vector(bx_phy_address addr);
   BX_MEM_SMF void    set_access_latency(Bit32u frequency_mhz, Bit64u ips);
@@ -188,7 +187,6 @@ public:
   virtual ~BX_MEM_C();
 
   BX_MEM_SMF void    init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz, Bit64u ips);
-  BX_MEM_SMF void    init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz);
   BX_MEM_SMF void    cleanup_memory(void);
 
   BX_MEM_SMF void    enable_smram(bool enable, bool restricted);
