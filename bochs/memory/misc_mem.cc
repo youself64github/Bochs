682f724434ef12b50271e49af0c81e8a95ef898a
youself64 <121099244+youself64github@users.noreply.github.com>
2026-07-30 22:54:08 +0300

    Revert

--- bochs/memory/misc_mem.cc
+++ bochs/memory/misc_mem.cc
@@ -51,16 +51,11 @@ BX_MEM_C::~BX_MEM_C()
   cleanup_memory();
 }
 
-void BX_MEM_C::init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz, Bit64u ips)
+void BX_MEM_C::init_memory(Bit64u guest, Bit64u host, Bit32u block_size)
 {
   unsigned idx, i;
 
-  BX_MEMORY_STUB_C::init_memory(guest, host, block_size, frequency_mhz, ips);
-void BX_MEM_C::init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz)
-{
-  unsigned idx, i;
-
-  BX_MEMORY_STUB_C::init_memory(guest, host, block_size, frequency_mhz);
+  BX_MEMORY_STUB_C::init_memory(guest, host, block_size);
 
   BX_MEM_THIS smram_available = false;
   BX_MEM_THIS smram_enable = false;
