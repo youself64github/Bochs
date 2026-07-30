56f544a858e3630d8e494113d9f149709720d515
youself64 <121099244+youself64github@users.noreply.github.com>
2026-07-30 22:22:33 +0300

    Add emulated memory speed frequency option

--- bochs/memory/misc_mem.cc
+++ bochs/memory/misc_mem.cc
@@ -51,11 +51,11 @@ BX_MEM_C::~BX_MEM_C()
   cleanup_memory();
 }
 
-void BX_MEM_C::init_memory(Bit64u guest, Bit64u host, Bit32u block_size)
+void BX_MEM_C::init_memory(Bit64u guest, Bit64u host, Bit32u block_size, Bit32u frequency_mhz, Bit64u ips)
 {
   unsigned idx, i;
 
-  BX_MEMORY_STUB_C::init_memory(guest, host, block_size);
+  BX_MEMORY_STUB_C::init_memory(guest, host, block_size, frequency_mhz, ips);
 
   BX_MEM_THIS smram_available = false;
   BX_MEM_THIS smram_enable = false;
