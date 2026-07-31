c575bdd83163788272276a43388b338e59a71662
youself64 <121099244+youself64github@users.noreply.github.com>
2026-07-31 06:41:17 +0300

    Fix vgalatency_ticks calculation type casting

--- bochs/iodev/display/vgacore.cc
+++ bochs/iodev/display/vgacore.cc
@@ -103,7 +103,7 @@ void bx_vgacore_c::init(void)
   BX_VGA_THIS pci_enabled = 0;
   Bit32u ips = SIM->get_param_num("ips")->get();
   BX_VGA_THIS vgafrequency_hz = SIM->get_param_num("vga_core_freq", base)->get();
-  BX_VGA_THIS vgalatency_ticks = (Bit32u) (ips / (Bit64u(vgafrequency_hz) * 1));
+  BX_VGA_THIS vgalatency_ticks = (Bit32u) (ips / (Bit32u(vgafrequency_hz) * 1));
   BX_INFO(("VGA speed frequency = %u Hz, latency = %u CPU ticks per VGA access",
       BX_VGA_THIS vgafrequency_hz, BX_VGA_THIS vgalatency_ticks));
   
